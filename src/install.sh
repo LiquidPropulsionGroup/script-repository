d3374d00c47fb6b24582426b2d19f0303abb25c8
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-27 01:48:08 -0800

    install improved

--- src/install.sh
+++ src/install.sh
@@ -3,12 +3,12 @@
 #   - Brian Almaguer (@balmabrian)
 
 # Set colors
-red=$'\e[1;31m' &
-grn=$'\e[1;32m' &
-blu=$'\e[1;34m' &
-mag=$'\e[1;35m' &
-cyn=$'\e[1;36m' &
-white=$'\e[0m' &
+red=$'\e[1;31m' 
+grn=$'\e[1;32m' 
+blu=$'\e[1;34m' 
+mag=$'\e[1;35m' 
+cyn=$'\e[1;36m' 
+white=$'\e[0m' 
 
 # Update
 echo "$red ==> $mag Updating the raspberry pi :-) $white"
