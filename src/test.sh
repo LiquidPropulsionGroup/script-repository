4365287690dd1981704e5905d2876dcf59a6e446
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-27 10:28:49 -0800

    sleep added for correct testing

--- src/test.sh
+++ src/test.sh
@@ -4,9 +4,11 @@ sudo apt-get update >> /dev/null 2>&1 &
 echo -ne '==========>          (50%)\r'
 wait
 
+sleep 10
 sudo apt-get -y upgrade >> /dev/null 2>&1 &
 echo -ne '===================> (90%)\r'
 wait
 
+sleep 10
 echo -ne '====================>(100%)\r'
 echo done
