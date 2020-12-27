573fc99bfa0701ad6fa51ea51ad66f207531d0db
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-27 10:22:49 -0800

    fix yes flag

--- src/test.sh
+++ src/test.sh
@@ -4,7 +4,7 @@ sudo apt-get update >> /dev/null 2>&1 &
 echo -ne '==========>          (50%)\r'
 wait
 
-sudo apt-get upgrade >> /dev/null 2>&1 &
+sudo apt-get -y upgrade >> /dev/null 2>&1 &
 echo -ne '===================> (90%)\r'
 wait
 
