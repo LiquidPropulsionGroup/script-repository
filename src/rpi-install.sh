9d784e383937a68e89c363350322a339d07c0b0f
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-26 19:46:03 -0800

    Update rpi-install.sh

--- src/rpi-install.sh
+++ src/rpi-install.sh
@@ -9,7 +9,7 @@ sudo apt-get update && sudo apt-get -y upgrade
 # Installing tools
 sudo apt install -y ctags gcc git make neovim nodejs npm python3-pip
 sudo npm install -g npm@latest
-sudo npm install -g import-js
+sudo npm install -g import-js --unsafe-perm
 sudo npm install -g eslint neovim typescript typescript-formatter
 sudo pip3 install isort pylint yapf
 ## Getting EngineWebServer
@@ -18,7 +18,7 @@ cd EngineWebServer
 npm install
 ## Installing Spacevim
 cd ~
-mkdir /home/pi/.SpaceVim.d/
+sudo mkdir /home/pi/.SpaceVim.d/
 echo "" > /home/pi/.SpaceVim.d/init.toml
-curl -sLf https://spacevim.org/install.sh | bash
 curl https://raw.githubusercontent.com/LiquidPropulsionGroup/script-repository/main/src/init.toml > /home/pi/.Spacevim.d/init.toml
+curl -sLf https://spacevim.org/install.sh | bash
