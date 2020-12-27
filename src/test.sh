0405264684184cf95b030f620f24d61ebc3ee91a
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-27 10:33:52 -0800

    new loading bar test

--- src/test.sh
+++ src/test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update >> /dev/null 2>&1 &
-echo -ne '==========>          (50%)\r'
+echo -ne '| ==========>           | (50%)\r'
 wait
 
 sleep 10
 sudo apt-get -y upgrade >> /dev/null 2>&1 &
-echo -ne '===================> (90%)\r'
+echo -ne '| ===================>  | (90%)\r'
 wait
 
 sleep 10
-echo -ne '====================>(100%)\r'
-echo done
+echo -ne '| ====================> | (100%) Done\r'
