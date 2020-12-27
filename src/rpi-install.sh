1ad4b304660342d2ec18d87021452e0560277c0d
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-26 19:04:11 -0800

    Create rpi-install.sh

---
+++ src/rpi-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Author: 
+#     - Brian Almaguer
+#     - Ruthvik Chowdary
+
+# Update and install
+sudo apt-get update && sudo apt-get -y upgrade
+
+# Installing tools
+sudo apt install -y ctags gcc git make neovim nodejs npm python3-pip
+sudo npm install -g npm@latest
+sudo npm install -g import-js
+sudo npm install -g eslint neovim typescript typescript-formatter
+sudo pip3 install isort pylint yapf
+## Getting EngineWebServer
+git clone https://github.com/LiquidPropulsionGroup/EngineWebServer.git
+cd EngineWebServer
+npm install
+## Installing Spacevim
+cd ~
+curl -sLf https://spacevim.org/install.sh | bash
+curl https://raw.githubusercontent.com/LiquidPropulsionGroup/script-repository/main/src/init.toml > /home/pi/.Spacevim.d/
