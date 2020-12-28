3970e5b7036db5f1f1aa1ee6b1938471f9520e7e
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-27 16:04:28 -0800

    Update rpi-install.sh

--- src/rpi-install.sh
+++ src/rpi-install.sh
@@ -58,7 +58,7 @@ Fancy-Tools () {
 
 # Print LPG Setup
 LPG-Banner () {
-toilet -f 3d -F border LPG Setup | lolcat
+toilet -f 3d LPG Setup | lolcat
   echo ""
 }
 
@@ -101,28 +101,28 @@ apt-get-pak () {
 
 #Installing npm packages
 npm-pak () {
-  echo "$red =========>$mag Installing npm packages $red<========= $white"
-  echo -ne "\r$red ==> $mag Installing npm npm@latest ..............$white"
+  echo "$red ==========>$mag Installing npm packages $red<========== $white"
+  echo -ne "\r$red ==> $mag Installing npm npm@latest ................$white"
   Progress 6
   npm install -g npm@latest >> /dev/null 2>&1 & 
   wait
-  echo -ne "\r$red ==> $mag Installing npm import-js ...............$white"
+  echo -ne "\r$red ==> $mag Installing npm import-js .................$white"
   Progress 12
   npm install -g import-js --unsafe-perm >> /dev/null 2>&1 &  
   wait
-  echo -ne "\r$red ==> $mag Installing npm eslint ..................$white"
+  echo -ne "\r$red ==> $mag Installing npm eslint ....................$white"
   Progress 18
   npm install -g eslint >> /dev/null 2>&1 & 
   wait
-  echo -ne "\r$red ==> $mag Installing npm neovim ..................$white"
+  echo -ne "\r$red ==> $mag Installing npm neovim ....................$white"
   Progress 24
   npm install -g neovim >> /dev/null 2>&1 & 
   wait
-  echo -ne "\r$red ==> $mag Installing npm typescript ..............$white"
+  echo -ne "\r$red ==> $mag Installing npm typescript ................$white"
   Progress 30
   npm install -g typescript >> /dev/null 2>&1 & 
   wait
-  echo -ne "\r$red ==> $mag Installing npm typescript-formatter ....$white"
+  echo -ne "\r$red ==> $mag Installing npm typescript-formatter ......$white"
   Progress 39
   npm install -g typescript-formatter >> /dev/null 2>&1 & 
   wait
@@ -133,14 +133,14 @@ npm-pak () {
 Install-SpaceVim () {
   mkdir /home/pi/.SpaceVim.d/ && echo "" > /home/pi/.SpaceVim.d/init.toml
   curl https://raw.githubusercontent.com/LiquidPropulsionGroup/script-repository/main/src/init.toml > /home/pi/.SpaceVim.d/init.toml
-  curl -sLf https://spacevim.org/install.sh | bash >> /dev/null & 
+  curl -sLf https://spacevim.org/install.sh | bash >> /dev/null 2>&1 & 
   wait
   nvim +ProcInstall +qall &
   wait
   echo ""
 
   # Installing EngineWebServer
-  toilet -f 3d -F border Installing EngineWebServer | lolcat
+  toilet -f 3d Installing | lolcat && toilet -f 3d EngineWebServer | lolcat
   echo -ne "\r$red ==> $mag Getting Repository ......................$white"
   Progress 20
   git clone https://github.com/LiquidPropulsionGroup/EngineWebServer.git >> /dev/null 2>&1 &  
