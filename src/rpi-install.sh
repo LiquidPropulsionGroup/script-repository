77258c1ac701f402965199ab8537d03bd512aee3
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2022-11-17 03:19:34 -0800

    ?

--- src/rpi-install.sh
+++ src/rpi-install.sh
@@ -172,14 +172,14 @@ NetworkInstall () {
 Docker-Preinstall () {
   # Install docker-compose
   curl -fsSL https://get.docker.com -o get-docker.sh
-  echo -ne "\r$cynIf the installation fails here, then Docker still requires a restart to install"
+  echo -ne "\r$cyn If the installation fails here, then Docker still requires a restart to install"
   echo -ne "Use `sudo reboot` and then input the following sequence of commands:"
   echo -ne "sudo dpkg --configure -a"
   echo -ne "sudo sh get-docker.sh"
   echo -ne "sudo usermod -aG docker ${USER}"
   echo -ne "sudo apt install docker-compose"
   echo -ne "sudo systemctl enable docker"
-  echo -ne "Verify docker with `docker ps` and verify docker-compose by navigating to /EnginePythonServer and trying `docker-compose`$white"
+  echo -ne "Verify docker with `docker ps` and verify docker-compose by navigating to /EnginePythonServer and trying `docker-compose` $white"
   sudo sh get-docker.sh
   sudo dpkg --configure -a
   sudo sh get-docker.sh
